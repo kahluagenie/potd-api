341c49c31b48e09b8dbb2a718149e86aaf2b75c2
Oleg Kalugin <okalugin@gluenetworks.com>
2014-12-17 10:28:37 -0800

    refactor tests

--- test/gopro-test.js
+++ test/gopro-test.js
@@ -6,30 +6,40 @@ const enhancePrototypes = require('../config/extend-prototypes');
 
 enhancePrototypes();
 
-describe('getPhoto', function () {
+describe('gopro controller', function () {
     it('should return the correct response', function (done) {
-        var url = 'https://gopro.com/photos/photo-of-the-day/2014/12/17';
         var mockHtml = './test/resources/gopro-potd-2014-12-17.html';
-
-        var expectedPhoto = {
-            uri: 'http://cbcdn2.gp-static.com/uploads/photo_of_the_day/image/135179/full_height2x_G0027451.jpg',
-            title: 'Skydive over Bay of Acre Israel',
-            byline: 'by Yogev Kalmanovich'
-        };
 
         fs.readFile(mockHtml, function (err, html) {
             if (err) {
                 throw err;
             }
 
-            nock(url)
-                .get('')
-                .reply(200, html);
-
-            goproController({params: [{date: '2014-12-17'}]}, function (photo) {
-                assert.deepEqual(photo, expectedPhoto);
-                done();
-            });
+            assertGoproControllerReturnsCorrectPhoto(html, done);
         });
     });
 });
+
+function assertGoproControllerReturnsCorrectPhoto(html, done) {
+    var url = 'https://gopro.com/photos/photo-of-the-day/2014/12/17';
+    var expectedPhoto = {
+        uri: 'http://cbcdn2.gp-static.com/uploads/photo_of_the_day/image/135179/full_height2x_G0027451.jpg',
+        title: 'Skydive over Bay of Acre Israel',
+        byline: 'by Yogev Kalmanovich'
+    };
+
+    nock(url)
+        .get('')
+        .reply(200, html);
+
+    var request = {
+        params: [{
+            date: '2014-12-17'
+        }]
+    };
+
+    goproController(request, function (photo) {
+        assert.deepEqual(photo, expectedPhoto);
+        done();
+    });
+}
